eb94f4ba523310d0c291a0f63f84e1026f3e308f
Egor <hyinm2012@yandex.ru>
2025-08-06 14:28:13 +0300

    add Tag

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,26 +3,5 @@
 require_relative "hexlet_code/version"
 
 module HexletCode
-  class Error < StandardError; end
-    class Tag; end
-      class << self
-        def build(*arr, &block)
-          case arr[0]
-          when "div"
-            puts "<#{arr[0]}></#{arr[0]}>"
-          when "img"
-            arr[1].map do |key, value|
-              puts "<#{arr[0]} #{key}=\"#{value}\">"
-            end
-          when "input"
-            params = []
-            arr[1].map do |key, value|
-              params << "#{key}=\"#{value}\""
-            end
-            puts "<#{arr[0]} #{params[0]} #{params[1]}>"
-          when "label"
-            puts "<#{arr[0]}>#{block.call}</#{arr[0]}>"
-          end
-        end
-      end
+  autoload(:Tag, "hexlet_code/tag")
 end
---
+++ lib/hexlet_code/tag.rb
@@ -0,0 +1,22 @@
+class Tag
+  class << self
+    def build(*arr, &block)
+      case arr[0]
+      when "div"
+        puts "<#{arr[0]}></#{arr[0]}>"
+      when "img"
+        arr[1].map do |key, value|
+          puts "<#{arr[0]} #{key}=\"#{value}\">"
+        end
+      when "input"
+        params = []
+        arr[1].map do |key, value|
+          params << "#{key}=\"#{value}\""
+        end
+        puts "<#{arr[0]} #{params[0]} #{params[1]}>"
+      when "label"
+        puts "<#{arr[0]}>#{block.call}</#{arr[0]}>"
+      end
+    end
+  end
+end
